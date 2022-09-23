7ab2a9257a536315b31b41bcd85831537777a3ca
Ankitglobalia <anc.globaliasoft@gmail.com>
2022-09-23 16:17:02 +0530

    change viewfile clear comment

--- Searching/search/views.py
+++ Searching/search/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .Serializer import StudentSerializers
 from.models import Student
 from rest_framework.generics import ListAPIView
-# from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
 
 # Create your views here.
@@ -10,8 +9,6 @@ class studentapi(ListAPIView):
     queryset=Student.objects.all()
     serializer_class= StudentSerializers
     filter_backends=[OrderingFilter]
-    # search_fields=['city']
-    # search_fields=['name','city']
     ordering_fields=['name']
     
     
