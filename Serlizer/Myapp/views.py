58b987a7c7b7f07a58c6925157bdc09327880bb1
Ankitglobalia <anc.globaliasoft@gmail.com>
2022-09-23 16:47:47 +0530

    clear data

--- Serlizer/Myapp/views.py
+++ Serlizer/Myapp/views.py
@@ -6,33 +6,21 @@ from.Serilizer import StudentSerilizer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse,JsonResponse
 # Create your views here.
-# MODEL OBJECT SINGLE STUDENT dATA
 def Student_Detail(request,pk):
-
     st= Student.objects.get(id=pk)
-    # print('st:',st)
-
     Serilizer=StudentSerilizer(st)
-    # print('serilizer', Serilizer)
-    # print('Serilizer:',Serilizer.data)
     json_Data=JSONRenderer().render(Serilizer.data)
-    # # print('json_data:',json_Data)
     return HttpResponse(json_Data,content_type='application/json')
-    # return JsonResponse(Serilizer.data)
 
-# Queyset 
+
 def Student_list(request):
-
     st= Student.objects.all()
-    # print('st:',st)
-
     Serilizer=StudentSerilizer(st,many=True)
-    # print('serilizer', Serilizer)
-    # print('Serilizer:',Serilizer.data)
     json_Data=JSONRenderer().render(Serilizer.data)
-    # print('json_data:',json_Data)
     return HttpResponse(json_Data,content_type='application/json')
-    # return JsonResponse(Serilizer.data,safe=False)
+
+
+
 
 
 
